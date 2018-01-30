907166c5dcca68169d41967fcb7e09445960ace3
Mustaque Ahemmed <hellomstq@gmail.com>
2018-01-30 10:20:22 +0600

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!yahhhhhhhhhhhhhhhhh
+                    You are logged in!!
                 </div>
             </div>
         </div>
